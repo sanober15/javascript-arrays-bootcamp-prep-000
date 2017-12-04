55171a1a9f6707eaa36d53115d189f8c8037b2e0
sanober15 <sanober@sanober.me>
2017-12-04 05:36:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,16 @@ var ingredient5 = 'tomato'
 var ingredient6 = 'garlic'
 
 var chocolateBars = ['snickers','hundred grands','kitkat', 'skittles']
+
+function addElementToBeginningOfArray(a, b){
+  const a = a;
+  
+  var x = ['b',...a];
+  return x;
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){
+
+  a.unshift('b')
+  
+}
